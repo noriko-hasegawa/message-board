f03c8a39524640792edac981e2fd7c7852144487
noriko.hasegawa <pikadon_nh@yahoo.co.jp>
2021-06-14 01:58:36 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,18 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @messages = Message.all
+  end
+
+  def show
+    set_message
+  end
+
+  def new
+    @message = Message.new
+  end
+
   def create
     @message = Message.new(message_params)
 
@@ -11,12 +26,11 @@
   end
 
   def edit
-    @message = Message.find(params[:id])
+    set_message
   end
 
   def update
-    @message = Message.find(params[:id])
-
+    set_message
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -27,7 +41,7 @@
   end
 
   def destroy
-    @message = Message.find(params[:id])
+    set_message
     @message.destroy
 
     flash[:success] = 'Message は正常に削除されました'
@@ -36,7 +50,11 @@
 
   private
 
-  # Strong Parameter
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
   def message_params
     params.require(:message).permit(:content)
   end
+end
